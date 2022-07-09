560031dfa5db028f58b7b5ce8a37cedb00e92e38
danielgil <danistcruz@gmail.com>
2022-07-08 19:41:11 -0500

    se avanza mas en los ejercicios y se modifican los test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Kata8.execute());
+        System.out.println(Kata9.execute());
     /**    assertEquals(Kata1.execute().size(), 4);
         System.out.println("Kata 1 solved!");
 
--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -1,15 +1,12 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import model.Bookmark;
-import model.Movie;
 import model.MovieList;
 import util.DataUtil;
-
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -19,10 +16,20 @@ import java.util.Map;
 public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
-        ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl");
 
-        
+        return movieLists.stream()
+                .flatMap(e->e.getVideos().stream())
+                .map(e->{
+                    String urlBoxArt = e.getBoxarts().stream()
+                            .reduce((acumulador, object) -> {
+                                if (object.getHeight() + object.getWidth() < acumulador.getWidth() + acumulador.getHeight()) {
+                                    return object;
+                                } else {
+                                    return acumulador;
+                                }
+                            }).get().getUrl();
 
-        return null;
+                    return ImmutableMap.of("id", e.getId(), "title", e.getTitle(), "time", new Date(), "url", urlBoxArt);
+                }).collect(Collectors.toList());
     }
 }
